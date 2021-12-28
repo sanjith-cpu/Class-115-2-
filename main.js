4bdd0527468e45d7f3ac168f9fe5018197839a49
sanjith-cpu <74964859+sanjith-cpu@users.noreply.github.com>
2021-12-28 17:51:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ function setup() {
     video.hide();
 
     PoseNet = ml5.poseNet(video,modelLoaded);
+    PoseNet.on('pose',gotPoses);
 }
 function draw() {
     image(video,0,0,300,300);
@@ -20,4 +21,13 @@ function take_snapshot() {
 
 function modelLoaded() {
     console.log('PoseNet Is Initialized');
+}
+
+function gotPoses() {
+    if(results.length>0)
+    {
+        console.log(results);
+        console.log("nose x= " + results[0].pose.nose.x);
+        console.log("nose y= " + results[0].pose.nose.y);
+    }
 }
